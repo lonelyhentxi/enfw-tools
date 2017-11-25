89e06f0a8b39623e7561f50813d266567ecd5c53
lonelyhentai <master@evernightfireworks.com>
2017-11-25 22:23:00 +0800

    Initialize enfw-plugins 171125

---
+++ zhihushu-auto/zhihushu-auto.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         AutoZhihushu
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  shows how to use babel compiler
+// @author       lonelyhentai
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @match        http://online.zhihuishu.com/CreateCourse/learning/videoList?courseId=*
+// ==/UserScript==
+
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+    /* jshint ignore:end */
+    /* jshint esnext: false */
+    /* jshint esversion: 6 */
+    setInterval(function(){
+        let cancelButtons = document.querySelectorAll('.popbtn_cancel');
+        if(cancelButtons.length!==0){
+            let cancelButton = cancelButtons[0];
+            cancelButton.click();
+        }
+        let passTimes = document.querySelectorAll('.passTime');
+        if(passTimes.length!==0){
+            let passTime = passTimes[0];
+            if(/100.*%/.test(passTime.style.width)){
+                let videoList = document.querySelectorAll('.clearfix.video.children');
+                let current = document.getElementsByClassName('progressbar_box_tip')[0].parentNode;
+                videoList[[...videoList].indexOf(current)+1].click();
+            }
+        }
+        if(document.getElementsByClassName('speedBox')[0].style&&!/1\.5-1\.png/.test(document.getElementsByClassName('speedBox')[0].style['cssText']))
+        {
+            [...document.getElementsByClassName('speedTab15')[0].attributes].filter(val=>{return val.name==='rate';})[0].value = '1.5';
+            document.getElementsByClassName('speedTab15')[0].click();
+        }
+        if(document.getElementsByClassName('volumeNone').length!==1)
+        {
+            document.getElementsByClassName('volumeIcon')[0].click();
+        }
+    },1000);
+/* jshint ignore:start */
+]]></>).toString();
+var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+eval(c.code);
+/* jshint ignore:end */
