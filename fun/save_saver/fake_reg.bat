7dabe07b6943620404834f5d9133424117e46c08
lonelyhentai <master@evernightfireworks.com>
2020-01-07 08:30:00 +0800

    fix(fun::save_saver): fix that can't view script content on github with ascii encoding and chinese

--- fun/save_saver/fake_reg.bat
+++ fun/save_saver/fake_reg.bat
@@ -1,4 +1,4 @@
 set TARGET_DIR="%UserProfile%\Documents\HARUKAZE"
-set TARGET_LINK="%UserProfile%\Documents\HARUKAZE\【绿茶汉化组】野良与皇女与流浪猫之心２"
+set TARGET_LINK="%UserProfile%\Documents\HARUKAZE\NoraNeko2"
 if not exist %TARGET_DIR% md %TARGET_DIR%
 if not exist %TARGET_LINK% mklink /J %TARGET_LINK% "%cd%"
